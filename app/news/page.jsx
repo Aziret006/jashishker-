4e81895b99501dd583e6c350885c5097085dec30
aziret006 <aziretaalybaev2006@gmail.com>
2024-06-14 12:05:27 +0600

    ашч

--- app/news/page.jsx
+++ app/news/page.jsx
@@ -1,19 +1,17 @@
-import Footer from '@/components/Footer/Footer'
-import Haeder from '@/components/Haeder/Haeder'
-import React from 'react'
+import Footer from "@/components/Footer/Footer";
+import Haeder from "@/components/Haeder/Haeder";
+import React from "react";
 
-import s from './page.module.scss'
+import s from "./page.module.scss";
 const page = () => {
   return (
     <div>
-    <Haeder/>
-    <div className={s.news}>
-      
-    </div>
-    
-    <Footer/>
-      </div>
-  )
-}
+      <Haeder />
+      <div className={s.news}></div>
 
-export default page
+      <Footer />
+    </div>
+  );
+};
+
+export default page;
--- components/Haeder/Haeder.jsx
+++ components/Haeder/Haeder.jsx
@@ -1,31 +1,34 @@
-import React from 'react'
-import s from './page.module.scss'
-import Image from 'next/image'
-import { Manrope } from 'next/font/google';
+import React from "react";
+import s from "./page.module.scss";
+import Image from "next/image";
+import { Manrope } from "next/font/google";
 const manrope = Manrope({ subsets: ["latin"] });
 const Haeder = () => {
-    return (
-        <haeder className={`${manrope.className}  ${ s.haeder}`} >
-            <div className={s.logo}>
-                <Image src="/logo.svg" alt="logo" width={50} height={50} />
-                <p>ЖАШ <br />ИШКЕР</p>
-            </div>
+  return (
+    <haeder className={`${manrope.className}  ${s.haeder}`}>
+      <div className={s.logo}>
+        <Image src="/logo.svg" alt="logo" width={50} height={50} />
+        <p>
+          ЖАШ <br />
+          ИШКЕР
+        </p>
+      </div>
 
-            <nav className={s.nav}>
-                <ul>
-                    <li>О проекте</li>
-                    <li>Новости</li>
-                    <li>База знаний</li>
-                    <li>История успеха</li>
-                    <li>Справочная Enactus</li>
-                </ul>
-            </nav>
-            <div className={s.ButtonList}>
-             <p>Ru</p>
-             <button>Войти</button>
-            </div>
-        </haeder>
-    )
-}
+      <nav className={s.nav}>
+        <ul>
+          <li>О проекте</li>
+          <li>Новости</li>
+          <li>База знаний</li>
+          <li>История успеха</li>
+          <li>Справочная Enactus</li>
+        </ul>
+      </nav>
+      <div className={s.ButtonList}>
+        <p>Ru</p>
+        <button>Войти</button>
+      </div>
+    </haeder>
+  );
+};
 
-export default Haeder
+export default Haeder;
