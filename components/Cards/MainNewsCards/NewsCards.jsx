251706b287062f3b09b0bff0e6c2d2091484ee16
ernis2005 <nhtfxnhtfx.9@gmail.com>
2024-06-18 13:04:25 +0600

    fix

--- components/Cards/MainNewsCards/NewsCards.jsx
+++ components/Cards/MainNewsCards/NewsCards.jsx
@@ -23,7 +23,7 @@ const NewsCards = ({ data }) => {
             <svg xmlns="http://www.w3.org/2000/svg" width="4" height="4" viewBox="0 0 4 4" fill="none">
               <circle cx="2" cy="2" r="2" fill="#222222" />
             </svg>
-            <p style={{opacity: 0.7}}>{data.endDate}</p>
+            <p style={{ opacity: 0.7 }}>{data.endDate}</p>
           </span>
           <button><GoArrowUpRight /></button>
         </span>
