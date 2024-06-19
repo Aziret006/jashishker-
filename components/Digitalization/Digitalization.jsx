61adf628fc921688ab80a82df51643cf25c6b963
Bekmyrza uulu Janarbek <99035622+zhanarkadyrmatov@users.noreply.github.com>
2024-06-19 12:09:09 +0600

    janar

---
+++ components/Digitalization/Digitalization.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import s from './page.module.scss'
+import { Alex_Brush, Be_Vietnam_Pro, Manrope } from "next/font/google";
+
+const FontManrope = Manrope({
+  subsets: ["latin"],
+  weight: ["600", "400"], // specify the desired weight here
+});
+export default function Digitalization() {
+  return (
+    <div className={s.digitalization}>
+      <div className={s.container}>
+        <div className={s.digitalizationTitle}>
+          <h5 className={FontManrope.className}>
+            Какие цифровые навыки бывают?
+          </h5>
+          <p className={FontManrope.className}>
+            Выделим три ключевые группы навыков:
+          </p>
+        </div>
+        <div className={s.digitalizationContent}>
+          <div className=''>
+
+          </div>
+
+
+
+        </div>
+      </div>
+    </div>
+  )
+}
