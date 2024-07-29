bed2ef0f3fde406716c872788c7e1225139ecdef
aziret006 <aziretaalybaev2006@gmail.com>
2024-07-29 10:47:10 +0600

    fix

--- app/database/page.jsx
+++ app/database/page.jsx
@@ -1,11 +1,14 @@
 "use client";
-
 import React, { useState } from "react";
 import Footer from "@/components/Footer/Footer";
 import Haeder from "@/components/Haeder/Haeder";
 import styles from "./page.module.scss";
 import Image from "next/image";
 import { AiOutlineAppstore } from "react-icons/ai";
+import { IoIosMenu } from "react-icons/io";
+import PersonalDataForm from "@/components/Modal/page";
+import AboutMe from "@/components/AboutMe/page";
+// import PersonalDataForm from "@/components/Modal/page";
 
 const contacts = [
   {
@@ -166,10 +169,10 @@ const contacts = [
 ];
 
 const Page = () => {
+  const [data, setData] = useState(false);
   const [sortBy, setSortBy] = useState("newest");
   const [filterBy, setFilterBy] = useState("all");
   const [selectedContact, setSelectedContact] = useState(null);
-
   const sortedContacts = [...contacts].sort((a, b) => {
     if (sortBy === "newest") return b.id - a.id;
     if (sortBy === "oldest") return a.id - b.id;
@@ -181,126 +184,135 @@ const Page = () => {
     if (filterBy === "favorites") return contact.isFavorite;
     if (filterBy === "active") return contact.isActive;
   });
+  const [page, setPage] = useState("");
 
   return (
-    <div>
-      <Haeder />
-      <div className={styles.mainDataBase}>
-        <div className={styles.dataBaseCont}>
-          <div className={styles.baseTitle}>
-            <h1>База данных</h1>
-          </div>
-          <hr />
-          <div className={styles.topBar}>
-            <div className={styles.filterOptions}>
-              <button className={styles.filterButton}>
-                Добавить в базу себя
-              </button>
+    <>
+      <div>
+        <Haeder />
+        <div className={styles.mainDataBase}>
+          <div className={styles.dataBaseCont}>
+            <div className={styles.baseTitle}>
+              <h1>База данных</h1>
             </div>
-            <div className={styles.sortOptions}>
-              <select
-                value={sortBy}
-                onChange={(e) => setSortBy(e.target.value)}
-              >
-                <option value="newest">Newest First</option>
-                <option value="oldest">Oldest First</option>
-                <option value="alphabetical">Alphabetical</option>
-              </select>
-              <button>
-                <AiOutlineAppstore size={24} />
-              </button>
-              <button>
-                <img src="/big.svg" alt="" />
-              </button>
-            </div>
-          </div>
-          <div className={styles.container}>
-            <div className={styles.contactList}>
-              {filteredContacts.map((contact) => (
-                <div
-                  key={contact.id}
-                  className={styles.contactItem}
-                  onClick={() => setSelectedContact(contact)}
+            <hr />
+            <div className={styles.topBar}>
+              <div className={styles.filterOptions}>
+                <button
+                  onClick={() => setData(true)}
+                  className={styles.filterButton}
                 >
-                  <img
-                    src={contact.photo}
-                    alt={contact.name}
-                    className={styles.photo}
-                  />
-                  <div className={styles.details}>
-                    <div>
-                      <h3>{contact.name}</h3>
-                      <p>{contact.service}</p>
-                    </div>
-                    <p>{contact.email}</p>
-                    <p>{contact.phone}</p>
-                  </div>
-                </div>
-              ))}
-            </div>
-            {selectedContact && (
-              <div className={styles.contactDetail}>
-                <div className={styles.baseBack}>
-                  <img
-                    src={selectedContact.photo}
-                    alt={selectedContact.name}
-                    className={styles.photo}
-                  />
-                  <div className={styles.flexCenter}>
-                    <h2>{selectedContact.name}</h2>
-                    <p>{selectedContact.age}</p>
-                  </div>
-                </div>
-                <div className={styles.info}>
-                  <div>
-                    <div className={styles.icon}>
-                      <img src="/location.svg" alt="" />
-                    </div>
-                    <p>
-                      <strong>Город:</strong> {selectedContact.city}
-                    </p>
-                  </div>
-                  <div>
-                    <div className={styles.icon}>
-                      <img src="/phone.svg" alt="" />
-                    </div>
-                    <p>
-                      <strong>Номер телефона:</strong> {selectedContact.phone}
-                    </p>
-                  </div>
-                  <div>
-                    <div className={styles.icon}>
-                      <img src="/mail.svg" alt="" />
-                    </div>
-                    <p>
-                      <strong>E-mail:</strong> {selectedContact.email}
-                    </p>
-                  </div>
-                  <div>
-                    <div className={styles.icon}>
-                      <img src="/user-octagon.svg" alt="" />
-                    </div>
-                    <p>
-                      <strong>ИП:</strong> {selectedContact.services}
-                    </p>
-                  </div>
-                  <div>
-                    <div className={styles.icon}>
-                      <img src="/brifecase-tick.svg" alt="" />
-                    </div>
-                    <p>
-                      <strong>Вид деятельности:</strong>{" "}
-                      {selectedContact.activity}
-                    </p>
-                  </div>
-                </div>
+                  Добавить в базу себя
+                </button>
               </div>
-            )}
+              <div className={styles.sortOptions}>
+                <select
+                  value={sortBy}
+                  onChange={(e) => setSortBy(e.target.value)}
+                >
+                  <option value="newest">Newest First</option>
+                  <option value="oldest">Oldest First</option>
+                  <option value="alphabetical">Alphabetical</option>
+                </select>
+                <button onClick={handleAddToBaseClick}>
+                  <AiOutlineAppstore size={24} />
+                </button>
+                <button onClick={handleNewestFirstClick}>
+                  <IoIosMenu size={24} />
+                </button>
+              </div>
+            </div>
+            <div className={styles.container}>
+              <div className={styles.contactList}>
+                {filteredContacts.map((contact) => (
+                  <div
+                    key={contact.id}
+                    className={styles.contactItem}
+                    onClick={() => setSelectedContact(contact)}
+                  >
+                    <img
+                      src={contact.photo}
+                      alt={contact.name}
+                      className={styles.photo}
+                    />
+                    <div className={styles.details}>
+                      <div>
+                        <h3>{contact.name}</h3>
+                        <p>{contact.service}</p>
+                      </div>
+                      <p>{contact.email}</p>
+                      <p>{contact.phone}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              {selectedContact && (
+                <div className={styles.contactDetail}>
+                  <div className={styles.baseBack}>
+                    <img
+                      src={selectedContact.photo}
+                      alt={selectedContact.name}
+                      className={styles.photo}
+                    />
+                    <div className={styles.flexCenter}>
+                      <h2>{selectedContact.name}</h2>
+                      <p>{selectedContact.age}</p>
+                    </div>
+                  </div>
+                  <div className={styles.info}>
+                    <div>
+                      <div className={styles.icon}>
+                        <img src="/location.svg" alt="" />
+                      </div>
+                      <p>
+                        <strong>Город:</strong> {selectedContact.city}
+                      </p>
+                    </div>
+                    <div>
+                      <div className={styles.icon}>
+                        <img src="/phone.svg" alt="" />
+                      </div>
+                      <p>
+                        <strong>Номер телефона:</strong> {selectedContact.phone}
+                      </p>
+                    </div>
+                    <div>
+                      <div className={styles.icon}>
+                        <img src="/mail.svg" alt="" />
+                      </div>
+                      <p>
+                        <strong>E-mail:</strong> {selectedContact.email}
+                      </p>
+                    </div>
+                    <div>
+                      <div className={styles.icon}>
+                        <img src="/user-octagon.svg" alt="" />
+                      </div>
+                      <p>
+                        <strong>ИП:</strong> {selectedContact.services}
+                      </p>
+                    </div>
+                    F
+                    <div>
+                      <div className={styles.icon}>
+                        <img src="/brifecase-tick.svg" alt="" />
+                      </div>
+                      <p>
+                        <strong>Вид деятельности:</strong>{" "}
+                        {selectedContact.activity}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+      {data && <PersonalDataForm setData={setData} />}
+      {message && <AboutMe />}
+    </>
   );
 };
 
---
+++ components/Modal/page.jsx
@@ -0,0 +1,64 @@
+// components/PersonalDataForm.js
+import React from "react";
+import styles from "./page.module.scss";
+
+const PersonalDataForm = ({ setData }) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Здесь можно обработать отправку формы
+    setData(false); // Пример использования setData
+  };
+
+  return (
+    <div onClick={() => setData(false)} className={styles.modal}>
+      <div className={styles.formContainer}>
+        <h2 className={styles.title}>Личные данные</h2>
+        <div className={styles.photoSection}>
+          <div className={styles.photoPlaceholder}></div>
+          <button className={styles.photoButton}>Добавить фото</button>
+        </div>
+        <form className={styles.form}>
+          <div className={styles.row}>
+            <input
+              type="text"
+              placeholder="Введите ФИО"
+              className={styles.input}
+            />
+            <input type="date" className={styles.input} />
+          </div>
+          <div className={styles.row}>
+            <input
+              type="tel"
+              placeholder="Введите номер телефона"
+              className={styles.input}
+            />
+            <input
+              type="email"
+              placeholder="example@gmail.com"
+              className={styles.input}
+            />
+          </div>
+          <div className={styles.row}>
+            <select className={styles.input}>
+              <option>Местоположение</option>
+            </select>
+            <input type="file" className={styles.input} />
+          </div>
+          <div className={styles.row}>
+            <input type="text" placeholder="ИП" className={styles.input} />
+            <input
+              type="text"
+              placeholder="Вид деятельности"
+              className={styles.input}
+            />
+          </div>
+          <button type="submit" className={styles.submitButton}>
+            Отправить
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default PersonalDataForm;
