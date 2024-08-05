a7f11cacbe5a3806e300770574b2185b9b55b058
ernis2005 <nhtfxnhtfx.9@gmail.com>
2024-08-05 15:59:52 +0600

    fix

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -1,6 +1,7 @@
 import { Manrope } from "next/font/google";
 import "./globals.css";
 import { NextIntlClientProvider, useMessages } from "next-intl";
+import LiveChatScript from "@/components/LiveChatScript/LiveChatScript";
 
 const manrope = Manrope({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children, params: { locale } }) {
   const messages = useMessages();
   return (
     <html lang="en">
+      <LiveChatScript />
       <NextIntlClientProvider locale={locale} messages={messages}>
         <body className={manrope.className}>{children}</body>
       </NextIntlClientProvider>
