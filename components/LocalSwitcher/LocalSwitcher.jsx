1760452aef935f56c1435deb2677ddefea83eddf
mayors7 <zulpukarov.bekulan.pi9.1.8@gmail.com>
2024-11-26 14:06:20 +0600

    fix

--- components/LocalSwitcher/LocalSwitcher.jsx
+++ components/LocalSwitcher/LocalSwitcher.jsx
@@ -4,29 +4,28 @@ import React, { useState, useTransition } from "react";
 import s from "./page.module.scss";
 import { useLocale, useTranslation } from "next-intl";
 import { Manrope } from "next/font/google";
-import { useRouter, usePathname } from "@/navigation";
 
 const manrope = Manrope({ subsets: ["latin"] });
 
 const LocalSwitcher = () => {
   const locale = useLocale();
-  // const [select, setSelect] = useState();
-  // const [_, startTransition] = useTransition();
-  const router = useRouter();
-  const pathName = usePathname();
+  const [select, setSelect] = useState();
+  const [_, startTransition] = useTransition();
+  console.log(locale, "test222");
 
   const handleLocale = (e) => {
-    router.replace(pathName, { locale: e.target.value, scroll: false });
+    setSelect(e.target.value);
+    startTransition(() => {
+      window.location.href = `/${e.target.value}`;
+    });
   };
 
   return (
     <div className={s.LocalSwitcher}>
-      <label htmlFor="state"></label>{" "}
       <select
-        id="state"
         value={locale}
         onChange={(e) => handleLocale(e)}
-        className={`${s.select} ${s.manrope}`}
+        className={`${s.select} ${s.manrope} `}
       >
         <option value="ru">Ру</option>
         <option value="ky">Ку</option>
